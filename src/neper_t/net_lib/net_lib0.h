d8072fa0bafd82b54a003f8dff5cadad65b420bb
Romain Quey <quey@emse.fr>
2012-11-24 16:01:00 +0100

    Clean code.
    Removed unused net_lib and NSite directories.

--- src/neper_t/net_lib/net_lib0.h
+++
@@ -1,5 +0,0 @@
-/* This file is part of the 'neper' program. */
-/* Copyright (C) 2003-2012, Romain Quey. */
-/* See the COPYING file in the top-level directory. */
-
-extern void net_lib_geo_import (char*, char*, int, struct GEO*);
